0fa0498ec2c6d4823b9290821c3c0059ad969fd8
CRONighttrain <daswasdadwad1@gmail.com>
2025-09-03 11:58:57 -0700

    added comments and moved some stuff around

--- Tree.py
+++ Tree.py
@@ -4,7 +4,14 @@ import RowFillTree
 from Board import Board
 
 def node(board: Board, side_length_map: dict[int, int], count: int, filled_row_map: dict[int, list[str]]):
+    #if we can't place anything then we have found a solution
+    available_side_lengths = get_available_side_lengths(side_length_map)
+    if len(available_side_lengths) == 0:
+        board.print_board()
+        print()
+        return 1
     available_space = board.get_amount_of_open_space()
+    #if we have computed all possible variations for the next open row on the board, iterate through those
     if filled_row_map.keys().__contains__(available_space):
         for row in filled_row_map[available_space]:
             if RowFillTree.valid_row(row, side_length_map):
@@ -17,13 +24,9 @@ def node(board: Board, side_length_map: dict[int, int], count: int, filled_row_m
                     new_map[key] -= row_count[key]
                 count += node(new_board, new_map, 0,filled_row_map)
         return count
+    #if we haven't computed the possible variations for the next opening iterate through every possible block we can place
     else:
-        available_side_lengths = get_available_side_lengths(side_length_map)
         next_open = board.get_next_opening()
-        if len(available_side_lengths) == 0:
-            board.print_board()
-            print()
-            return 1
         for length in available_side_lengths:
             # filter out any case where the 1 is on the sides of the board as they cannot happen
             if length == 1 and ((next_open["y"] == 0 or next_open["y"] == (board.length - 1)) or (next_open["x"] == 0 or next_open["x"] == (board.length - 1))):
